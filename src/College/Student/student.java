9d6c432c5dca850ff30575b4b3aa8a6ca61000af
Your Name <Ankitabhattarai01>
2025-01-25 17:47:21 +0545

    Added initial changes to feature-grade-calculator

--- src/College/Student/student.java
+++ src/College/Student/student.java
@@ -1,5 +1,19 @@
 package College.Student;
 
-public class student {
+public class Student {
+    private String name;
+    private int age;
 
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
